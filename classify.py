80be380050c2d8489a1a828bfca35e07973b1392
Dan Pelis <dppelis@gmail.com>
2020-10-17 15:54:41 -0400

    Added csv write functionality to classify.py

--- classify.py
+++ classify.py
@@ -5,15 +5,13 @@ file_name_ratings=dataDir + 'output1.txt'
 ratings = open(file_name_ratings, 'r')
 
 with open('userSuggestions.csv','w') as f:
-    row = 'TrackID,Predictor'
+    row = 'TrackID,Predictor\n'
     f.write(row)
 
 trackID_vec=[0]*6
 albumRate_vec=[0]*6
 artistRate_vec=[0]*6
 genreRate_vec=[0]*6
-
-mybrainhurt=True
 lastUserID=-1
 
 
@@ -43,21 +41,20 @@ for line in ratings:
         total_ratings = {}
         for nn in range(0, 6):
             total_ratings[trackID_vec[nn]] = albumRate_vec[nn] * 0.5  + artistRate_vec[nn] * 0.35 + genreRate_vec[nn] * 0.15
-            
-        total_ratings = {k: v for k, v in sorted(total_ratings.items(), key=lambda item: item[1], reverse=True)}
 
-        print(list(total_ratings.keys())[0])
-        print(list(total_ratings.values()))
-        print(np.unique(list(total_ratings.values())))
-        
-        
+        total_ratings_sorted = total_ratings.copy()
+        total_ratings_sorted = {k: v for k, v in sorted(total_ratings.items(), key=lambda item: item[1], reverse=True)}
 
-        print(total_ratings)
-        exit()
-      
-        '''
+        # print(list(total_ratings.keys())[0])
+        # print(list(total_ratings.values()))
+        # print(np.unique(list(total_ratings.values())))
+        
+        suggest = [0,0,0,1,1,1]
+        for key in total_ratings_sorted.keys():
+            total_ratings[key] = suggest.pop()
+        
         with open('userSuggestions.csv','a') as f:
-            for nn in range(0, 6):
-                row = f'{userID}_{trackID_vec[nn]},{}'
+            for item_id, value in total_ratings.items():
+                row = f'{userID}_{item_id},{value}\n'
                 f.write(row)
-        '''
+        
